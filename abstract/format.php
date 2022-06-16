056d4bebf783492e24806e5c481e29890c731eb5
kibbang <007kibbang@gmail.com>
2022-06-16 19:10:47 +0900

    Abstract 1

---
+++ abstract/format.php
@@ -0,0 +1,19 @@
+<?php
+abstract class ParentClass
+{
+    public function a()
+    {
+        echo "a";
+    }
+
+    public abstract function b();
+
+}
+
+class ChildClass extends ParentClass
+{
+    public function b()
+    {
+        
+    }
+}
