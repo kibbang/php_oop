5dd516bc0b14606e59c223ba5dfbb92405774e0e
kibbang <007kibbang@gmail.com>
2022-06-16 13:28:21 +0900

    생성자(constructor)

--- oop/class_instance.php
+++ oop/class_instance.php
@@ -1,19 +1,19 @@
 <?php
 class myFileObject {
+    function __construct($fname) // 생성자: 인스턴스를 생성할때 생성자를 먼저 호출
+    {
+        $this->filename = $fname;
+    }
+
     function isFile(): bool
     {
         return is_file($this->filename);
     }
 }
-$file = new myFileObject();
-$file->filename = 'data.txt';
+$file = new myFileObject('data.txt');
+
 var_dump($file->isFile());
 var_dump($file->filename);
-
-$file2 = new myFileObject();
-$file2->filename = 'data2.txt';
-var_dump($file2->isFile());
-var_dump($file2->filename);
 
 // 클래스를 만드는 방법
 // 클래스 키워드 선언 후 클래스 이름 적는다 그리고 중괄호로 시작과 끝을 맺음
@@ -29,3 +29,9 @@ $file, $file2 : Instance
 isFile : method, behavior
 $this->filename : Instance variable, Instance field, Instance property
  */
+
+//============================================//
+// 생성자 : 인자값으로 $fname이 data.txt가 됨 그러면 생성자가 실행될때 $fname은 대입 연산자(=)로 인해
+// $this->>filename이 되고 그러면 이제 $this->filename 셋팅이 된것이다.
+// 생성자의 역할: 인스턴스가 만들어질 때 꼭 필요한 값을 인자로 넘겨 받을 수 있다. 인스턴스가 생성될때 해야 할 것 들을
+// 생성자에 적어두면 그대로 초기화가 된다.
